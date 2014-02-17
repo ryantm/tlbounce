25ef44988cff65e207f146813c91072d1f941166
Ryan Mulligan <ryan@ryantm.com>
2014-02-16 17:25:52 -0800

    add Clojure project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject tlbounce "0.1.0-SNAPSHOT"
+  :description "An IRC bouncer that logs messages into the talklibre message format."
+  :url "https://github.com/talklibre/tlbounce"
+  :license {:name "CC0 1.0 Universal"
+            :url "http://creativecommons.org/publicdomain/zero/1.0/legalcode"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ircparse "0.1.0-SNAPSHOT"]])
---
+++ test/tlbounce/core_test.clj
@@ -0,0 +1,7 @@
+(ns tlbounce.core-test
+  (:require [clojure.test :refer :all]
+            [tlbounce.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
