b227fe313a21ee085e9c9d8326793d925cc61a40
Ryan Mulligan <ryan@ryantm.com>
2014-03-29 19:19:38 -0700

    bump dependency versions

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "CC0 1.0 Universal"
             :url "http://creativecommons.org/publicdomain/zero/1.0/legalcode"}
   :main tlbounce.core
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ircparse "0.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ircparse "0.4.0"]
                  [ring "1.2.2"]
                  [org.clojure/tools.reader "0.8.3"]])
